8f93a69d028d0b67d5a23d6eb849bc5e15a8fe0f
jf5400 <43279688+jf5400@users.noreply.github.com>
2018-12-05 14:53:59 -0500

    Update Runner.java

--- src/Game/Runner.java
+++ src/Game/Runner.java
@@ -1,4 +1,20 @@
-package Game;
+package Rooms;
 
-public class Runner {
+public class Board {
+    private Room[][] map;
+    public Board(Room [][] map){
+        this.map=map;
+    }
+
+    public void print(){
+        String row="";
+        for(int i=0; i<map.length; i++){
+            row="";
+            for(int j=0; j<map[i].length; j++) {
+                row+=map[i][j].toString();
+            }
+            System.out.println(row);
+        }
+    }
+
 }
